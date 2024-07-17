eb28f37ebcde649184c0f73b73f0521343158166
askrdev <askrdev@gmail.com>
2024-07-18 03:39:35 +0700

    cat years dog years DONE

--- 8kyuKatas/180624_catyearsdogyears.py
+++ 8kyuKatas/180624_catyearsdogyears.py
@@ -16,8 +16,8 @@ humanYears >= 1
 humanYears are whole numbers only
 Cat Years
 15 cat years for first year
-+9 cat years for second year
-+4 cat years for each year after that
++9 cat years for second year | 24 years
++4 cat years for each year after that | third years is 28 
 Dog Years
 15 dog years for first year
 +9 dog years for second year
@@ -38,6 +38,17 @@ def fixed_tests():
         test.assert_equals(human_years_cat_years_dog_years(10), [10,56,64])
 """
 
+human_years = 3
+
 def human_years_cat_years_dog_years(human_years):
     # Your code here
-    return [0,0,0]
+    return [human_years,human_years *15,human_years * 15] if human_years == 1 else [human_years,15+9,15+9] if human_years == 2 else [human_years, 4*human_years +16 , 5 * human_years + 14]
+
+print(human_years_cat_years_dog_years(human_years))
+
+# *** Jawaban Simple ***
+# hy = 3
+# def human_years_cat_years_dog_years(hy):
+#     return [hy, 16 + 4 * hy, 14 + 5 * hy] if hy > 1 else [1,15,15]
+
+# print(human_years_cat_years_dog_years(hy))
